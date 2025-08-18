5d33ac080ea6cfa3f07fa2e437999bcf52dd8074
josebonini <jose.bonini.sanches@escola.pr.gov.br>
2025-08-18 09:02:23 -0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import {PALAVRAS_RUINS} from "./palavrasRuins"
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
@@ -12,6 +13,13 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
@@ -19,7 +27,6 @@ function processaTexto(texto) {
         return frequencias[p2] - frequencias[p1];
     }
 
-    console.log(ordenadas);
     return ordenadas.slice(0, 10);
 }
 
@@ -34,4 +41,15 @@ function contaFrequencias(palavras) {
         }
     }
     return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+  
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
